9502845a68905ef344bbec6565d2c6737ef03665
RSijelmass <R.Sijelmass@gmail.com>
2017-06-25 14:08:32 +0100

    Solves problem 12

--- lib/questions.rb
+++ lib/questions.rb
@@ -71,6 +71,11 @@ end
 # even numbers come first
 # so [1, 2, 3, 4, 5, 6] becomes [[2, 4, 6], [1, 3, 5]]
 def separate_array_into_even_and_odd_numbers(array)
+	evens = []; odds = [];
+	array.each do |num|
+		num % 2 == 0 ? (evens << num) : (odds << num)
+	end
+	[evens, odds]
 end
 
 # count the numbers of elements in an element which are palindromes
