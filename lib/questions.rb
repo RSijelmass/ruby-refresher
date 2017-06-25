6729cbc92e6d707a38608a9fd0f3f84f710e82b6
RSijelmass <R.Sijelmass@gmail.com>
2017-06-25 13:23:18 +0100

    Solves problem 1 and 2

--- lib/questions.rb
+++ lib/questions.rb
@@ -1,9 +1,11 @@
 # keep only the elements that start with an a
 def select_elements_starting_with_a(array)
+	array.select { |element| element[0] == 'a' }
 end
 
 # keep only the elements that start with a vowel
 def select_elements_starting_with_vowel(array)
+	array.select { |element| ['a', 'e', 'i', 'o', 'u'].include? (element[0]) }
 end
 
 # remove instances of nil (but NOT false) from an array
